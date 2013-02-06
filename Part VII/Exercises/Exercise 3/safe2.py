9512f91e9d071ddf101c76154e172c7c1668be97
Alexey <Alexey.N.Chenyshov@gmail.com>
2013-02-06 14:28:07 +0400

    Exercises for part VII

---
+++ Part VII/Exercises/Exercise 3/safe2.py
@@ -0,0 +1,14 @@
+﻿#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+import sys, traceback
+
+def safe(entry, *args):
+	try:
+		entry(*args)
+	except:
+		traceback.print_exc()
+		print('Got', sys.exc_info()[0], sys.exc_info()[1])
+		
+import oops
+safe(oops.oops)
---
+++ Part VII/Exercises/Exercise 4/main.py
@@ -0,0 +1,16 @@
+﻿#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+# Поиск наибольшего файла в единственном каталоге
+import os, glob
+if sys.platform[:3] == 'win':
+	dirname = r'C:\Python30\Lib'
+else:
+	dirname = '/usr/lib/python'
+allsizes = []
+allpy = glob.glob(dirname + os.sep + '*.py')
+for filename in allpy:
+	filesize = os.path.getsize(filename)
+	allsizes.append((filesize, filename))
+allsizes.sort()
+print(allsizes[-1:])
