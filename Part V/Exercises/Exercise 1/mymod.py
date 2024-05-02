16454749da660b87314cb438f81140078ec58a6c
TechnoDexx <itshark@mail.ru>
2024-05-02 07:54:47 +0300

    Exercises for part V

--- Part V/Exercises/Exercise 1/mymod.py
+++ Part V/Exercises/Exercise 1/mymod.py
@@ -3,17 +3,21 @@
 
 import sys
 
+
 def countLines(file):
 	file.seek(0)
 	return len(file.readlines())
+
 
 def countChars(file):
 	file.seek(0)
 	return len(file.read())
 
+
 def test(name):
 	file = open(name)
 	print('Lines: ' + str(countLines(file)))
 	print('Chars: ' + str(countChars(file)))
+
 
 test(sys.argv[1])
